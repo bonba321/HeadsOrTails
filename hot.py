b8a1d3fca432bfe320e394bc870bf59a0f899880
bonba321 <bonba.2019@icloud.com>
2026-06-26 18:14:20 +0900

    User name added

---
+++ hot.py
@@ -0,0 +1,17 @@
+import random
+
+heads = 0
+tails = 0
+
+print("Tossing a coin...")
+
+for i in range(1,4):
+    toss = random.choice(["Heads","Tails"])
+    print(f"Round {i}: {toss}")
+
+    if toss == "Heads":
+        heads += 1
+    else:
+        tails += 1
+
+print(f"Heads: {heads}, Tails: {tails}")
